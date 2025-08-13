d33ebf12237f4fe8a285718967d940aa0d6d7fb4
AC42027 <diego_melgarejo@goodyear.com>
2025-08-13 08:52:37 -0400

    proyecto ya en servidor

--- vdf_project/settings.py
+++ vdf_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hbg0%jz@i3#ed3p_)35ps$b_x%y&19twusi-i-0i*$pbcrit7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.107.194.110', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIcFIles_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- vdf_project/urls.py
+++ vdf_project/urls.py
@@ -1,10 +1,14 @@
 # vdf_project/urls.py
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from vdf_monitor.views import VDFListAPIView, LecturaListAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('api/vdfs/', VDFListAPIView.as_view(), name='vdf-list'),
     path('api/lecturas/', LecturaListAPIView.as_view(), name='lectura-list'),
 ]
